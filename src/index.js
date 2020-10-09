a63e4836a95fb285d3afc0e62e421833fa727901
Utku Gultopu <ugultopu@gmail.com>
2020-10-09 12:04:24 -0400

    Add a variable and nested argument to query

--- src/index.js
+++ src/index.js
@@ -18,17 +18,28 @@ const client = new ApolloClient({
 // "gql" variable here) gets called with the template literal.
 //
 // More info at Template Literals page on MDN.
-const GET_ORGANIZATION = gql`
-  {
-    organization(login: "the-road-to-learn-react") {
+const GET_REPOSITORIES_OF_ORGANIZATION = gql`
+  query($organization: String!) {
+    organization(login: $organization) {
       name
       url
+      repositories(first: 5) {
+        edges {
+          node {
+            name
+            url
+          }
+        }
+      }
     }
   }
 `;
 
 client
   .query({
-    query: GET_ORGANIZATION,
+    query: GET_REPOSITORIES_OF_ORGANIZATION,
+    variables: {
+      organization: 'the-road-to-learn-react',
+    },
   })
   .then(console.log);
