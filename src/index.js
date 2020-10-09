929fbcb270367389a4cd00a6ec21425314f592ab
Utku Gultopu <ugultopu@gmail.com>
2020-10-09 11:54:43 -0400

    Set up Apollo Client
    Send the first request and get the first response.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
+import 'cross-fetch/polyfill';
+import ApolloClient, { gql } from 'apollo-boost';
 import 'dotenv/config';
 
-const userCredentials = { firstname: 'Robin' };
-const userDetails = { nationality: 'German' };
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+      }
+    })
+  }
+});
 
-const user = {
-  ...userCredentials,
-  ...userDetails,
-};
+// Below is a "tagged template". The expression (usually a function)
+// preceding a template literal (where that expression is the imported
+// "gql" variable here) gets called with the template literal.
+//
+// More info at Template Literals page on MDN.
+const GET_ORGANIZATION = gql`
+  {
+    organization(login: "the-road-to-learn-react") {
+      name
+      url
+    }
+  }
+`;
 
-console.log(user);
-
-console.log(process.env.SOME_ENV_VARIABLE);
+client
+  .query({
+    query: GET_ORGANIZATION,
+  })
+  .then(console.log);
