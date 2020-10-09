9d8a79ca5ec8223b5a83df7b50006e89d56013f7
Utku Gultopu <ugultopu@gmail.com>
2020-10-09 12:29:30 -0400

    Implement mutation with Apollo Client and GraphQL

--- src/index.js
+++ src/index.js
@@ -35,11 +35,31 @@ const GET_REPOSITORIES_OF_ORGANIZATION = gql`
   }
 `;
 
+const ADD_STAR = gql`
+  mutation AddStar($repositoryId: ID!) {
+    addStar(input: { starrableId: $repositoryId }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
+
 client
   .query({
     query: GET_REPOSITORIES_OF_ORGANIZATION,
     variables: {
       organization: 'the-road-to-learn-react',
+    },
+  })
+  .then(console.log);
+
+client
+  .mutate({
+    mutation: ADD_STAR,
+    variables: {
+      repositoryId: 'MDEwOlJlcG9zaXRvcnk2MzM1MjkwNw==',
     },
   })
   .then(console.log);
